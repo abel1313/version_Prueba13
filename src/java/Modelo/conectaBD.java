19ba069ce6c48a8b1f0c50c9149b1174f85dbf08
abel_ <abel_@Trece-13>
2020-03-02 23:49:24 -0600

    co

--- src/java/Modelo/conectaBD.java
+++ src/java/Modelo/conectaBD.java
@@ -5,10 +5,32 @@
  */
 package Modelo;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author abel_
  */
 public class conectaBD {
+   private Connection conexion=null;
+   
+    public Connection conexionBD(){
+   
+       try {
+           Class.forName("com.mysql.jdbc.Driver");
+       } catch (ClassNotFoundException ex) {
+           Logger.getLogger(conectaBD.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       try {
+           conexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/wingsColin","root","");
+       } catch (SQLException ex) {
+           Logger.getLogger(conectaBD.class.getName()).log(Level.SEVERE, null, ex);
+       }   
+    return conexion;
+    }
+
+  
     
 }
